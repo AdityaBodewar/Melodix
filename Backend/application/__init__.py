cc58ba9402031fb867770edb8c62a69a03a2905c
Aditya <adityabodewar835@gmail.com>
2025-12-05 16:13:24 +0530

    cors added

--- Backend/application/__init__.py
+++ Backend/application/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from application.db import conn
 from application.cloud import cloud_Connectione
-
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
 dbconnection=conn()
 cloud_Connectione()
 
