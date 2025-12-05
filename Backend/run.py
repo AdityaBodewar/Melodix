6c91dea92598cd19c4857f7dc4761ccce966fbc9
Aditya <adityabodewar835@gmail.com>
2025-12-05 17:46:23 +0530

    port added

--- Backend/run.py
+++ Backend/run.py
@@ -1,5 +1,9 @@
 from application.__init__ import app
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=os.getenv("PORT"),debug=True )
