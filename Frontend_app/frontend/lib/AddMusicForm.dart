92d1e6b734dfc330913a472005629e554204730a
priyanshu bhandare <pbhandare239@gmail.com>
2025-12-05 17:34:39 +0530

    dependencie added

--- Frontend_app/frontend/lib/AddMusicForm.dart
+++ Frontend_app/frontend/lib/AddMusicForm.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
-import 'api_service.dart';
+import 'package:frontend/ApiService .dart';
 
 class AddMusicForm extends StatefulWidget {
   const AddMusicForm({super.key});
