ff0e2cffa7578f7e925a6fc2ad5ca44e7be62900
Aditya <adityabodewar835@gmail.com>
2025-12-09 10:59:52 +0530

    home page created

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import GetAllMusic from '../Components/GetAllMusic'
+import NavBar from '../Components/NavBar'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+    
+    <div className="nav">
+      <NavBar/>
+    </div>
+    <div className="container flex ">
+
+       <div className="head h-150 w-30 border-2 border-blue-100 bg-amber-600 rounded-2xl">
+        <div className='overflow-y-auto h-full'>
+          <GetAllMusic/>
+        </div>
+
+
+
+    </div>
+    <div className="ml-5 w-300 h-150 border-black border-2 rounded-2xl ">
+      <div className="overflow-x-auto w-full "  >
+      <GetAllMusic/>
+      </div>
+    </div>
+
+
+
+    <div className="footer"></div> 
+    </div>
+      
+    
+    
+    </>
   )
 }
 
