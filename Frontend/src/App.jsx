a39f3f7bb0b5c568189877d6af6528d552702a07
Aditya <adityabodewar835@gmail.com>
2025-12-09 11:04:38 +0530

    app.jsx modified

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import AddMusic from './Components/AddMusic'
-import GetAllMusic from './Components/GetAllMusic'
 import HomePage from './Pages/HomePage'
 
 const App = () => {
   return (
-  
-    
+   
   <HomePage/>
     
   )
