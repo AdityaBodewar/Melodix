c81ac2bbcbdcdcfedc1899f2ca61c89726efcc07
Aditya <adityabodewar835@gmail.com>
2025-12-20 21:52:53 +0530

    removed port

--- Backend/run.py
+++ Backend/run.py
@@ -1,9 +1,7 @@
 from application.__init__ import app
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=os.getenv("PORT"),debug=True )
+    app.run()
